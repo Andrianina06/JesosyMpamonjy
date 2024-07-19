ee4d117a1744406ee3e2870cdd6fa2c03fe3feca
Zo <andrianinarandriamanantena@gmail.com>
2024-07-19 14:47:20 +0300

    commit all

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,8 +1,6 @@
 @extends('template')
 @section('title', 'Acceuil')
 @section('content')
-<<<<<<< HEAD
-<<<<<<< HEAD
     <h1>Les derniers évènements</h1>
     <div class="row">
         @foreach ($evenements as $evenement)
@@ -42,9 +40,6 @@
             </div>
         @endforeach
     </div>
-=======
-=======
->>>>>>> 74c9c38 (commit all)
 <header class="header-2">
     <div class="page-header min-vh-75 relative" style="background-image: url('../assets/img/jmp/cross.jpg'); opacity: 0.5">
       <span class="mask bg-gradient-faded-dark opacity-2"></span>
