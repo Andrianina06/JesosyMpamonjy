9e1c82437d36421c278908823b4addb52e30dcea
Zo <andrianinarandriamanantena@gmail.com>
2024-07-19 15:40:27 +0300

    commit all

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,58 +1,19 @@
 @extends('template')
 @section('title', 'Acceuil')
 @section('content')
-    <h1>Les derniers évènements</h1>
-    <div class="row">
-        @foreach ($evenements as $evenement)
-            <div class="col-3">
-                <div class="card mb-4 rounded-3 shadow-sm">
-                    <div class="card-header py-3">
-                        <h4 class="my-0 fw-normal text-center">{{ $evenement->exemple->exemple }}</h4>
-                    </div>
-                    <div class="card-body">
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li> {{ $evenement->lieu->lieu }}</li>
-                            <li>Debut : {{ $evenement->date_debut }}</li>
-                            <li>Fin : {{ $evenement->date_fin }}</li>
-                        </ul>
-                        <a href="{{ route('userEvenement.show', ['evenement'=>$evenement])}}"><button type="button" class="w-100 btn btn-lg btn-outline-primary">Détails</button></a>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-    </div>
-    <h1>Quelques églises</h1>
-    <div class="row">
-        @foreach ($eglises as $eglise)
-            <div class="col-3">
-                <div class="card mb-4 rounded-3 shadow-sm">
-                    <div class="card-header py-3">
-                        <h4 class="my-0 fw-normal text-center">{{ $eglise->lieu->lieu }}</h4>
-                    </div>
-                    <div class="card-body">
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li>{{ $eglise->lieu->lieu }}</li>
-                            <li>Pst {{ $eglise->personne->prenom }}</li>
-                        </ul>
-                        <a href="{{ route('userEglise.show', ['eglise'=>$eglise])}}"><button type="button" class="w-100 btn btn-lg btn-outline-primary">Détails</button></a>
-                    </div>
-                </div>
-            </div>
-        @endforeach
-    </div>
-<header class="header-2">
-    <div class="page-header min-vh-75 relative" style="background-image: url('../assets/img/jmp/cross.jpg'); opacity: 0.5">
-      <span class="mask bg-gradient-faded-dark opacity-2"></span>
+<header class="header-2 mt-7">
+    <div class="page-header min-vh-75 relative" style="background-image: url('../assets/img/jmp/cross.jpg'); opacity: 0.7">
+      <span class="mask bg-gradient-primary opacity-2"></span>
       <div class="container">
         <div class="row">
           <div class="col-lg-7 text-center mx-auto">
-            <h1 class="text-center text-white pt-3 mt-n5">FIANGONANA JESOSY MPAMONJY</h1>
+            <h1 class="text-center text-white pt-3 mt-n5" style="opacity: 0.7;">FIANGONANA JESOSY MPAMONJY</h1>
           </div>
         </div>
       </div>
     </div>
 </header>
-<div class="card card-body blur  shadow-blur mt-7">
+<div class="card card-body blur shadow-blur mt-7 mx-3 mx-md-4 mt-n6">
     <section class="pt-3 pb-4" id="count-stats">
         <div class="container">
             <h1>Les derniers évènements</h1>
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,91 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
-<<<<<<< HEAD
     <link rel="stylesheet" href="/css/bootstrap.min.css">
-=======
     <link rel="stylesheet" href="/assets/css/material-kit.min.css">
->>>>>>> 74c9c38 (commit all)
-=======
-    <link rel="stylesheet" href="/assets/css/material-kit.min.css">
->>>>>>> 74c9c38 (commit all)
     <title>@yield('title')</title>
 </head>
 @php
     $route = request()->route()->getName(); 
 @endphp
 <body>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <div style="background: #b50000b8">
-        <nav class="navbar navbar-expand navbar-dark py-3" aria-label="Second navbar example">
-            <div class="container-fluid">
-                <h1 class="navbar-brand" href="#">JESOSY MPAMONJY</h1>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="justify-content-end" id="navbarsExample02">
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a @class(['nav-link', 'active'=>str_contains($route, 'userEglise.')]) href="{{ route('userEglise.index') }}">Eglise</a>
-                        </li>
-                        <li class="nav-item">
-                            <a @class(['nav-link', 'active'=>str_contains($route, 'userEvenement.')]) href="{{ route('userEvenement.index') }}">Evenement</a>
-                        </li>
-                    </ul>
-                </div>
+    <div class="container position-sticky z-index-sticky top-0">
+        <div class="row">
+            <div class="col-12">
+                <nav class="navbar navbar-expand-lg border-radius-xl top-0 z-index-fixed shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
+                <div class="container-fluid px-0">
+                    <a class="navbar-brand font-weight-bolder ms-sm-3" href="{{ route('user.index') }}" rel="tooltip"  
+                    title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">JESOSY MPAMONJY</a>
+                    <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon mt-2">
+                        <span class="navbar-toggler-bar bar1"></span>
+                        <span class="navbar-toggler-bar bar2"></span>
+                        <span class="navbar-toggler-bar bar3"></span>
+                        </span>
+                    </button>
+                    <div class="collapse navbar-collapse pt-3 pb-2 py-lg-0 w-100" id="navigation">
+                        <ul class="navbar-nav navbar-nav-hover ms-auto">
+                            <li class="nav-item dropdown dropdown-hover mx-2">
+                                <a @class(["nav-link ps-2 d-flex cursor-pointer align-items-center", 'active'=>str_contains($route, 'affectation.')]) href="{{ route('affectation.index') }}" id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false">Affectation</a>
+                            </li>
+                            <li class="nav-item dropdown dropdown-hover mx-2">
+                                <a @class(["nav-link ps-2 d-flex cursor-pointer align-items-center", 'active'=>str_contains($route, 'eglise.')]) href="{{ route('eglise.index') }}" id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false">Eglise</a>
+                            </li>
+                            <li class="nav-item dropdown dropdown-hover mx-2">
+                                <a @class(["nav-link ps-2 d-flex cursor-pointer align-items-center", 'active'=>str_contains($route, 'evenement.')]) href="{{ route('evenement.index') }}" id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false">Evenement</a>
+                            </li>
+                            <li class="nav-item dropdown dropdown-hover mx-2">
+                                <a @class(["nav-link ps-2 d-flex cursor-pointer align-items-center", 'active'=>str_contains($route, 'pasteur.')]) href="{{ route('personne.index') }}" id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false">Pasteur</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
-        </nav>
+        </div>
     </div>
     <div class="container mt-2">
         @yield('content')
     </div>
-=======
-=======
->>>>>>> 74c9c38 (commit all)
-    <div class="container position-sticky z-index-sticky top-0"><div class="row"><div class="col-12">
-    <nav class="navbar navbar-expand-lg  blur border-radius-xl top-0 z-index-fixed shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
-      <div class="container-fluid px-0">
-          <a class="navbar-brand font-weight-bolder ms-sm-3" href="{{ route('user.index') }}" rel="tooltip"  
-          title="Designed and Coded by Creative Tim" data-placement="bottom" target="_blank">JESOSY MPAMONJY</a>
-          <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon mt-2">
-              <span class="navbar-toggler-bar bar1"></span>
-              <span class="navbar-toggler-bar bar2"></span>
-              <span class="navbar-toggler-bar bar3"></span>
-              </span>
-          </button>
-          <div class="collapse navbar-collapse pt-3 pb-2 py-lg-0 w-100" id="navigation">
-              <ul class="navbar-nav navbar-nav-hover ms-auto">
-                  <li class="nav-item dropdown dropdown-hover mx-2">
-                      <a @class(["nav-link ps-2 d-flex cursor-pointer align-items-center", 'active'=>str_contains($route, '')]) id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false"></a>
-                  </li>
-                  <li class="nav-item dropdown dropdown-hover mx-2">
-                      <a @class(["nav-link ps-2 d-flex cursor-pointer align-items-center", 'active'=>str_contains($route, '')]) id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false"></a>
-                  </li>
-                  <li class="nav-item dropdown dropdown-hover mx-2">
-                      <a @class(["nav-link ps-2 d-flex cursor-pointer align-items-center", 'active'=>str_contains($route, '')]) id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false"></a>
-                  </li>
-                  <li class="nav-item dropdown dropdown-hover mx-2">
-                      <a @class(["nav-link ps-2 d-flex cursor-pointer align-items-center", 'active'=>str_contains($route, '')]) id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false"></a>
-                  </li>
-                  <li class="nav-item dropdown dropdown-hover mx-2">
-                      <a @class(["nav-link ps-2 d-flex cursor-pointer align-items-center", 'active'=>str_contains($route, '')]) id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false"></a>
-                  </li>
-                  <li class="nav-item dropdown dropdown-hover mx-2">
-                      <a @class(["nav-link ps-2 d-flex cursor-pointer align-items-center", 'active'=>str_contains($route, '')]) id="dropdownMenuPages" data-bs-toggle="dropdown" aria-expanded="false"></a>
-                  </li>
-              </ul>
-          </div>
-        </div>
-    </nav>
-<div class="container mt-2">
-@yield('content')
-</div>
-<<<<<<< HEAD
->>>>>>> 74c9c38 (commit all)
-=======
->>>>>>> 74c9c38 (commit all)
 </body>
 </html>
