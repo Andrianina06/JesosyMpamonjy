3438268854fa855dac94e4bb9a4c624e48600808
Zo <andrianinarandriamanantena@gmail.com>
2024-07-19 15:41:56 +0300

    commit all

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,51 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<<<<<<< HEAD
-<<<<<<< HEAD
     <link rel="stylesheet" href="/css/bootstrap.min.css">
-=======
     <link rel="stylesheet" href="./assets/css/material-kit.css">
->>>>>>> 74c9c38 (commit all)
-=======
     <link rel="stylesheet" href="./assets/css/material-kit.css">
->>>>>>> 74c9c38 (commit all)
     <title>Fiangonana Jesosy Mpamonjy</title>
 </head>
 @php
     $route = request()->route()->getName(); 
 @endphp
 <body>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <div style="background: #b50000b8">
-        <nav class="navbar navbar-expand navbar-dark py-3" aria-label="Second navbar example">
-            <div class="container-fluid">
-            <h1 class="navbar-brand" href="#">JESOSY MPAMONJY</h1>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample02" aria-controls="navbarsExample02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        
-            <div class="justify-content-end" id="navbarsExample02">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a @class(['nav-link']) href="">Affectation</a>
-                    </li>
-                </ul>
-            </div>
-            </div>
-        </nav>
-    </div>
-    <div class="container">
-        <h1>Tongasoa!</h1>
-        <p>Ity tranonkala ity dia natao ho an'ny mpino ao amin'ny fiangonana Jesosy Mpamonjy, hampitana ireo vaovao-mpiangonana, toy ny fitoriana filazantsara,fanosorana mpitandrina,sy ireo hetsika maro hafa ihany koa. Koa manasa anao hitsidika ny tranonkala</p>
-        <div class="justify-content-center d-flex">
-            <a href="{{ route('user.index')}}"><button class="btn btn-danger">Hitsidika</button></a>
-        </div>
-    </div>
-=======
-=======
->>>>>>> 74c9c38 (commit all)
     <header class="header-2">
         <div class="page-header min-vh-100 relative" style="background-image: url('./assets/img/bg2.jpg')">
           <span class="mask bg-gradient-primary opacity-2"></span>
@@ -66,9 +30,5 @@
           </div>
         </div>
     </header>
-<<<<<<< HEAD
->>>>>>> 74c9c38 (commit all)
-=======
->>>>>>> 74c9c38 (commit all)
 </body>
 </html>
